51e7750907d1c7ec8c22fee3d9c76d69e2af512e
Sudarsan Govinda Reddy <sudarsang@virtusapolaris.com>
2016-09-21 18:42:06 +0530

    initial commit - added getVideo and getFile

---
+++ README.md
@@ -0,0 +1,5 @@
+##Twitch2Tube Bot
+
+This bot will attempt to provide youtube mirrors for twitch.tv links in Reddit.
+
+I am beginning to write this with /r/Dota2 in mind because I can't open the damned twitch links properly from my ancient phone.
---
+++ video.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getFileName(downloadLink string) string {
+	hash := md5.New()
+	io.WriteString(hash, downloadLink)
+
+	var hashStr = hex.EncodeToString(hash.Sum(nil)) + ".mp4" //need to change this
+
+	fmt.Printf("%#v", hashStr)
+	return hashStr
+}
+
+func downloadVids(downloadLink string, done chan<- bool) {
+	resp, err := http.Get(downloadLink)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	var file = getFileName(downloadLink)
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		out, err := os.Create(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer out.Close()
+
+		io.Copy(out, resp.Body)
+
+		fmt.Println("saved file ", file)
+	} else {
+		fmt.Println(file, " already exists")
+	}
+
+	done <- true
+}
