9c7fc5ceb21b38573d7955812b717a721d88393f
Sudarshan-ArchLinux <sudar.theone@gmail.com>
2016-09-22 15:03:22 +0530

    finished up auth a bit

--- auth.go
+++ auth.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
 
 	"golang.org/x/oauth2"
 )
 
-func auth(CID, CSecret, scope1 string) {
+func auth(CID, CSecret, scope1 string) (*http.Client, error) {
 	ctx := context.Background()
 	conf := &oauth2.Config{
 		ClientID:     CID,
@@ -18,5 +20,16 @@ func auth(CID, CSecret, scope1 string) {
 			TokenURL: "https://youtube.com/o/oauth2/token",
 		},
 	}
-	fmt.Println("test")
+
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+
+	tok, err := conf.Exchange(ctx, code)
+
+	return conf.Client(ctx, tok), err
 }
